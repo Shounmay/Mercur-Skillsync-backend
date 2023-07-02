f5ce1f5f4aabd635999ee376640be4abaad4fe1c
Shounmay <shounmaydarsh131@gmail.com>
2023-07-03 00:16:20 +0530

    backend features of authentication and gigs

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/AuthRoutes.js';
+import { gigRoutes } from './routes/GigRoutes.js';
+import { orderRoutes } from './routes/OrderRoutes.js';
+import { messageRoutes } from './routes/MessageRoutes.js';
+import { dashboardRoutes } from './routes/DashboardRoutes.js';
+dotenv.config();
+
+const app = express();
+//console.log(process.env);
+const port = process.env.PORT;
+
+app.use('/uploads', express.static('uploads'));
+app.use('/uploads/profiles', express.static('uploads/profiles'));
+
+app.use(
+	cors({
+		origin: [process.env.PUBLIC_URL],
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+		credentials: true,
+	})
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/gigs', gigRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/dashboard', dashboardRoutes);
+app.listen(port, () => {
+	console.log(`app is running on port ${port}`);
+});
+
+// import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const app: express.Application = express();
+// const port: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
+// app.listen(port, () => {
+//   console.log(`app is running on port ${port}`);
+// });
